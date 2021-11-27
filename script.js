0576f50e55376d440fb6b1c419b3ee687d3f5bd9
mytechthingz <mytechthingz@gmail.com>
2021-11-27 13:41:40 +0000

    remaining changes for the project

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    let interest = ((principal * rate * years) /100);
+    let total = parseInt(principal) + interest;
+    let date = new Date().getFullYear() + parseInt(years);
+    document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br> at an interest rate of <mark>${rate}%</mark>.<br>You will receive an amount of <mark>${total}</mark>,<br>in the year <mark>${date}</mark>`;
+    console.log(`${rate} ${principal} ${years}`)
+}
+
+function updateRate(){
+    document.getElementById("rate_val").innerHTML = document.getElementById("rate").value;
 }
         
+function principalCheck(){
+    let principal = document.getElementById("principal").value;
+    if(principal <=0 ){
+        alert("Enter a positive number");
+    }
+}
